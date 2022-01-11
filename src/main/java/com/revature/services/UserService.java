3fe44a9fb6b9a5602fa11ec42b9d80f4c3a93b83
rfloyd01 <lynrdskynrd05@gmail.com>
2022-01-11 18:46:50 -0500

    improved verbiage for some comments

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -16,33 +16,34 @@ public class UserService {
 
     public int newUser(UserRequest u) {
 
-        //we need to implement logic here to make sure that the information passed to create the new user works.
-        //A binary number will be used to track all errors that occured while tyring to make a user. If there are
+        //This function handles some logic when creating a new user, namely, making sure correct information was passed.
+        //A binary number will be used to track all errors that occur while tyring to make a user. If there are
         //no issues then this binary number will be a 0
 
-        //There are 7 possible reasons for the creation request to fail, each bit is responsible for storing one
-        //of these errors. The password errors will be "guilty until proven innocent"
-        int errorCode = 0b1111000; //anything wrong with the user creation attempt will be stored in this error code
+        //There are 7 possible reasons for the creation request to fail at this stage, each bit is responsible for storing one
+        //of these errors. Certain errors will be "guilty until proven innocent" (i.e. the password will be assumed to
+        //have no lowercase letters until the first lowercase letter is actually seen)
+        int errorCode = 0b1111000;
 
         //First, we have to check if the desired userType is valid (ideally when creating a new user there would be
-        //some kind of check box on the front end to make sure you only select a valid user, however, this app isn't
-        //going to have a front end and is only passed a string so this check needs to happen)
+        //some kind of check box on the front end to make sure you only select a valid userType, however, this app isn't
+        //going to have a front end and is only passed a string so this check is necessary)
         switch (u.userType) {
             case "Customer":
-                break; //this is fine
             case "Employee":
-                break; //this is fine
             case "Admin":
-                break; //this is fine
+                break; //these are all fine options
             default:
                 errorCode |= 0b1;
+                //Note: After finding this first error it would be possible to just break out of the user creation
+                //method, however, we let this method complete to see if there are any other errors to save the user
+                //the hassle of multiple attempts when creating an account
         }
 
-        //Second, we need to access the DAO layer and see if the desired username passed in through 'u' is available
-        //(i.e. doesn't already exist in the database)
+        //Next, we need to access the DAO layer to see if the selected username already exists or not
         if (!validUsernameDAO(u.username)) errorCode |= 0b10;
 
-        //Next, we need to make sure that the password passed in with the 'u' parameter meets all of the strict
+        //Finally, we need to make sure that the password passed in with the 'u' parameter meets the strict password
         //requirements. These requirements are:
         //1. password must be at least 10 characters in length
         //2. password must feature at least 1 lowercase letter
@@ -51,10 +52,8 @@ public class UserService {
         //5. password must have at least 1 special character.
         if (u.password.length() < 10) errorCode |= 0b100;
 
-        //look through the password one letter at a time and see if each criterion has been met. Track
-        //the criteria using a 4 bit number. If each bit is set from a 0 to a 1 then all the criteria
-        //have been met
-
+        //look through the password one character at a time and see if each criterion has been met.
+        //If each bit is set from a 1 to a 0 in the errorCode then all the criteria have been met
         for (int i = 0; i < u.password.length(); i++) {
             char c = u.password.charAt(i);
 
@@ -65,15 +64,14 @@ public class UserService {
             else errorCode &= 0b111111; //special character criteria met TODO - may want to consider limiting which characters count here
 
             if (errorCode == 0) {
-                //as soon as our errorCode has been reduced to 0 we are cleared to create a new user
-                //we invoke the UserFactory. If for something there's a failure in the DAO layer when
+                //as soon as our errorCode has been reduced to 0 we are cleared to create a new user so
+                //we invoke the UserFactory. If for some reason there's a failure in the DAO layer when
                 //calling the user factory it's error code will be added here, indicating that the
                 //user wasn't actually created.
                 return errorCode | userDAO.addUser(u);
             }
         }
 
-        System.out.println("Error code is " + errorCode);
         //if we get to this point in the loop then something was wrong with the UserRequest, return the full error code
         //without creating a user
         return errorCode;
